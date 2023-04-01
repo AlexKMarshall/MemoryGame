a72711beedbdc6255c2e20e6b596ca3e4e62ce04
AlexKMarshall <alexmarshall@qogita.com>
2023-04-01 14:01:20 +0100

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+// state needed
+// array of items on the board
+// item has value, turned, matched, which player matched it
+
+function App() {
+  return (
+    <div>
+      <h1>Memory</h1>
+    </div>
+  );
+}
+
+export default App;
