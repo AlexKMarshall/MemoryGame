c2ee73a18fa28d7ee3668ca12757b9b0ce82f51b
AlexKMarshall <alexmarshall@qogita.com>
2023-04-07 12:31:22 +0100

    feat: add moves count

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { useEffect, useReducer, useState } from "react";
 import { getRandomNumbers, shuffleArray } from "./utils";
 import * as styles from "./App.css";
+import { useInterval } from "./useInterval";
 
 type GameState = {
+  state: "idle" | "inProgress" | "complete";
   cards: CardState[];
+  moves: number;
+  /**
+   * The time in milliseconds since the game started
+   */
+  timer: number;
 };
 
 type CardState = {
@@ -21,8 +28,15 @@ type CheckMatchAction = {
 type FlipNonMatchesDownAction = {
   type: "flipNonMatchesDown";
 };
+type IncrementTimeAction = {
+  type: "incrementTime";
+};
 
-type GameActions = FlipUpAction | CheckMatchAction | FlipNonMatchesDownAction;
+type GameActions =
+  | FlipUpAction
+  | CheckMatchAction
+  | FlipNonMatchesDownAction
+  | IncrementTimeAction;
 
 function gameReducer(state: GameState, action: GameActions): GameState {
   switch (action.type) {
@@ -37,12 +51,16 @@ function gameReducer(state: GameState, action: GameActions): GameState {
       // flip card
       const newState = {
         ...state,
+        state: "inProgress",
         cards: [...state.cards],
-      };
+      } satisfies GameState;
       newState.cards[action.cardIndex] = {
         ...card,
         state: "faceUp",
       };
+      if (getFaceUpCardCount(newState.cards) === numberInMatch) {
+        newState.moves++;
+      }
       return newState;
     }
     case "checkMatch": {
@@ -52,21 +70,19 @@ function gameReducer(state: GameState, action: GameActions): GameState {
       const faceUpCards = state.cards.filter((card) => card.state === "faceUp");
       const values = faceUpCards.map((card) => card.value);
       const isMatch = values.every((value) => value === values[0]);
-      if (isMatch) {
-        return {
-          ...state,
-          cards: state.cards.map((card) => {
-            if (card.state === "faceUp") {
-              return {
-                ...card,
-                state: "matched",
-              };
-            }
-            return card;
-          }),
-        };
+      if (!isMatch) return state;
+      const newState = { ...state };
+
+      newState.cards = newState.cards.map((card) => ({
+        ...card,
+        state: card.state === "faceUp" ? "matched" : card.state,
+      }));
+
+      if (getIsGameComplete(newState.cards)) {
+        newState.state = "complete";
       }
-      return state;
+
+      return newState;
     }
     case "flipNonMatchesDown": {
       if (getFaceUpCardCount(state.cards) !== numberInMatch) {
@@ -89,6 +105,11 @@ function gameReducer(state: GameState, action: GameActions): GameState {
           }),
         };
       }
+    }
+
+    case "incrementTime": {
+      if (state.state !== "inProgress") return state;
+      return { ...state, timer: state.timer + 1000 };
     }
     default:
       return state;
@@ -117,6 +138,9 @@ function getInitialGameState(size: 4 | 6 = 4) {
       value: number,
       state: "faceDown",
     })),
+    moves: 0,
+    state: "idle",
+    timer: 0,
   } satisfies GameState;
 }
 
@@ -129,20 +153,28 @@ function getFaceUpCardCount(cards: CardState[]) {
   return cards.filter((card) => card.state === "faceUp").length;
 }
 
+function getIsGameComplete(cards: CardState[]) {
+  return cards.every((card) => card.state === "matched");
+}
+
 function App() {
   const [gameState, dispatch] = useReducer(gameReducer, getInitialGameState());
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const timeoutId = setTimeout(() => {
       dispatch({ type: "checkMatch" });
     }, 1000);
-    return () => clearTimeout(timer);
-  });
+    return () => clearTimeout(timeoutId);
+  }, [gameState.cards]);
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const timeoutId = setTimeout(() => {
       dispatch({ type: "flipNonMatchesDown" });
     }, 1000);
-    return () => clearTimeout(timer);
-  });
+    return () => clearTimeout(timeoutId);
+  }, [gameState.cards]);
+  useInterval(
+    () => dispatch({ type: "incrementTime" }),
+    gameState.state === "inProgress" ? 1000 : null
+  );
 
   const handleCardSelect = (index: number) => {
     dispatch({ type: "checkMatch" });
@@ -153,23 +185,28 @@ function App() {
   return (
     <main className={styles.main}>
       <h1 className={styles.heading}>memory</h1>
-      <ul className={styles.cardGrid}>
-        {gameState.cards.map((card, index) => (
-          <li key={index}>
-            <button
-              onClick={() => handleCardSelect(index)}
-              className={styles.cardButton[card.state]}
-            >
-              {card.state === "matched" ? (
-                <span>{card.value}!</span>
-              ) : card.state === "faceUp" ? (
-                card.value
-              ) : null}
-            </button>
-          </li>
-        ))}
-      </ul>
-      <p>Time</p>
+      {getIsGameComplete(gameState.cards) ? (
+        <div>Game complete</div>
+      ) : (
+        <ul className={styles.cardGrid}>
+          {gameState.cards.map((card, index) => (
+            <li key={index}>
+              <button
+                onClick={() => handleCardSelect(index)}
+                className={styles.cardButton[card.state]}
+              >
+                {card.state === "matched" ? (
+                  <span>{card.value}!</span>
+                ) : card.state === "faceUp" ? (
+                  card.value
+                ) : null}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+      <p>Time: {gameState.timer / 1000}</p>
+      <p>Moves: {gameState.moves}</p>
     </main>
   );
 }
---
+++ src/useInterval.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect, useRef } from "react";
+
+type Callback = () => void;
+export function useInterval(callback: Callback, delayMs: number | null) {
+  const savedCallback = useRef<Callback>();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      if (savedCallback.current) {
+        savedCallback.current();
+      }
+    }
+    if (delayMs !== null) {
+      let id = setInterval(tick, delayMs);
+      return () => clearInterval(id);
+    }
+  }, [delayMs]);
+}
