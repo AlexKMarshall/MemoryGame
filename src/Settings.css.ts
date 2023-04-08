4d07a2c014b0afae9816382a42f2970e653e1b01
AlexKMarshall <alexmarshall@qogita.com>
2023-04-08 16:31:16 +0100

    style: settings page for tablet

--- src/Settings.css.ts
+++ src/Settings.css.ts
@@ -1,5 +1,4 @@
 import { style } from "@vanilla-extract/css";
-import { buttonBase, buttonPrimary } from "./Home.css";
 
 export const main = style({
   padding: "1.5rem",
@@ -8,12 +7,22 @@ export const main = style({
   display: "grid",
   gridTemplateRows: "1fr auto 1fr",
   alignItems: "center",
+  "@media": {
+    "screen and (min-width: 768px)": {
+      padding: "3rem",
+    },
+  },
 });
 
 export const heading = style({
   fontSize: "2rem",
   color: "hsl(0deg 0% 99%)",
   textAlign: "center",
+  "@media": {
+    "screen and (min-width: 768px)": {
+      fontSize: "2.5rem",
+    },
+  },
 });
 
 export const form = style([
@@ -21,11 +30,22 @@ export const form = style([
     padding: "1.5rem",
     backgroundColor: "hsl(0deg 0% 99%)",
     borderRadius: 10,
+    "@media": {
+      "screen and (min-width: 768px)": {
+        padding: "3.5rem",
+        borderRadius: 20,
+      },
+    },
   },
 ]);
 
 export const legend = style({
   color: "hsl(203deg 22% 55%)",
+  "@media": {
+    "screen and (min-width: 768px)": {
+      fontSize: "1.25rem",
+    },
+  },
 });
 
 export const radioGroup = style({
@@ -33,20 +53,53 @@ export const radioGroup = style({
   gridAutoFlow: "column",
   gridAutoColumns: "1fr",
   gap: "0.75rem",
-});
-
-export const radio = style([
-  buttonBase,
-  {
-    backgroundColor: "hsl(203deg 28% 79%)",
-    color: "hsl(0deg 0% 99%)",
-
-    selectors: {
-      ":checked + &": {
-        backgroundColor: "hsl(205deg 30% 27%)",
-      },
+  "@media": {
+    "screen and (min-width: 768px)": {
+      gap: "2rem",
     },
   },
-]);
+});
 
-export const submitButton = buttonPrimary;
+// how does this relate to the button secondary variant?
+export const radio = style({
+  paddingBlock: "0.75rem",
+  paddingInline: "1.25rem",
+  border: "none",
+  borderRadius: 999,
+  display: "flex",
+  justifyContent: "center",
+  backgroundColor: "hsl(203deg 28% 79%)",
+  color: "hsl(0deg 0% 99%)",
+
+  "@media": {
+    "screen and (min-width: 768px)": {
+      fontSize: "1.5rem",
+    },
+  },
+
+  selectors: {
+    ":checked + &": {
+      backgroundColor: "hsl(205deg 30% 27%)",
+    },
+  },
+});
+
+// Possibly just a larger button variant?
+export const submitButton = style({
+  paddingBlock: "0.75rem",
+  paddingInline: "1.25rem",
+  fontSize: "1.125rem",
+  border: "none",
+  borderRadius: 999,
+  display: "flex",
+  justifyContent: "center",
+  backgroundColor: "hsl(37deg 98% 54%)",
+  color: "hsl(0deg 0% 99%)",
+  "@media": {
+    "screen and (min-width: 768px)": {
+      paddingBlock: "1rem",
+      paddingInline: "1.75rem",
+      fontSize: "2rem",
+    },
+  },
+});
