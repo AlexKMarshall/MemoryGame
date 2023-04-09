41c4fdebd6400aef4857aa39a6934e415a9d27ba
AlexKMarshall <alexmarshall@qogita.com>
2023-04-09 20:48:12 +0100

    fix: forward ref to button and button link components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, ComponentProps } from "react";
+import { ButtonHTMLAttributes, ComponentProps, forwardRef } from "react";
 import * as styles from "./Button.css";
 import type { ButtonVariants } from "./Button.css";
 import { Link } from "react-router-dom";
@@ -9,11 +9,13 @@ export type ButtonProps = Omit<
 > &
   ButtonVariants;
 
-// TODO: forward refs
-
-export function Button({ size, color, ...props }: ButtonProps) {
-  return <button {...props} className={styles.Button({ size, color })} />;
-}
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  function Button({ size, color, ...props }, ref) {
+    return (
+      <button {...props} className={styles.Button({ size, color })} ref={ref} />
+    );
+  }
+);
 
 type LinkProps = ComponentProps<typeof Link>;
 
@@ -22,6 +24,10 @@ export type ButtonLinkProps = Omit<LinkProps, "className"> & ButtonVariants;
 /**
  * A Link with button styles
  */
-export function ButtonLink({ size, color, ...props }: ButtonLinkProps) {
-  return <Link {...props} className={styles.Button({ size, color })} />;
-}
+export const ButtonLink = forwardRef<HTMLAnchorElement, ButtonLinkProps>(
+  function ButtonLink({ size, color, ...props }, ref) {
+    return (
+      <Link {...props} className={styles.Button({ size, color })} ref={ref} />
+    );
+  }
+);
